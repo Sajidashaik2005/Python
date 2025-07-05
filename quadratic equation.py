9c7b89e0e67c16945764ddcf002d0ff65aecb4c7
Sajidashaik2005 <shaiksajida1448@gmail.com>
2025-07-05 19:07:39 +0530

    Add files via upload

---
+++ quadratic equation.py
@@ -0,0 +1,6 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:")) 
+x=int(input("enter the value of x:")) 
+result=a*(x**2)+b*x+c
+print("The result of the equation is",result)
